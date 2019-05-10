795b8dabdbfaf22470907e66a1c61cbff662d216
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-05-09 19:38:48 -0500

    what is functions

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -23,15 +23,15 @@ class TabLink {
 
   select() {
     // Get all of the elements with the tabs-link class
-    // const links;
-
+    const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
-    // Array.from(links).forEach();
-
-    // Add a class named "tabs-link-selected" to this link
-    // this.element;
+      Array.from(links).forEach(link => {
+      link.classList.remove('.tabs-link-selected')});
     
+    // Add a class named "tabs-link-selected" to this link
+    this.element.classList.add("tabs-link-selected");
     // Call the select method on the item associated with this link
+    ???.select();
 
   }
 }
@@ -44,12 +44,13 @@ class TabItem {
 
   select() {
     // Select all ".tabs-item" elements from the DOM
-    // const items;
+    const items = document.querySelectorAll('.tabs-item');
 
     // Remove the class "tabs-item-selected" from each element
-    
+    Array.from(items).forEach(link => {
+      link.classList.remove('.tabs-item-selected')});
     // Add a class named "tabs-item-selected" to this element
-    //this.element;
+    this.element.classList.add("tabs-item-selected");
   }
 }
 
