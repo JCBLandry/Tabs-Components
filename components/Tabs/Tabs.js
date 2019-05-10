ed55a1a69e9af04cee7769ad62a3f183a8705743
Justin Landry <32848504+JCBLandry@users.noreply.github.com>
2019-05-09 20:05:20 -0500

    MVP

--- components/Tabs/Tabs.js
+++ components/Tabs/Tabs.js
@@ -13,7 +13,7 @@ class TabLink {
   
     
     // Using the Item element, create a new instance of the TabItem class
-    this.tabItem = new TabItem;
+    this.tabItem = new TabItem(this.itemElement);
     
     // Add a click event listener on this instance, calling the select method on click
     this.element.addEventListener('click',
@@ -26,12 +26,12 @@ class TabLink {
     const links = document.querySelectorAll('.tabs-link');
     // Using a loop or the forEach method remove the 'tabs-link-selected' class from all of the links
       Array.from(links).forEach(link => {
-      link.classList.remove('.tabs-link-selected')});
+      link.classList.remove('tabs-link-selected')});
     
     // Add a class named "tabs-link-selected" to this link
     this.element.classList.add("tabs-link-selected");
     // Call the select method on the item associated with this link
-    ???.select();
+    this.tabItem.select();
 
   }
 }
@@ -48,7 +48,8 @@ class TabItem {
 
     // Remove the class "tabs-item-selected" from each element
     Array.from(items).forEach(link => {
-      link.classList.remove('.tabs-item-selected')});
+      link.classList.remove('tabs-item-selected')});
+      
     // Add a class named "tabs-item-selected" to this element
     this.element.classList.add("tabs-item-selected");
   }
